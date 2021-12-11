2541cbff43b6acd62b9cb3f11b25b98d9cb03402
zoid23 <luigi.vitelli@igenius.ai>
2021-12-11 19:07:49 +0100

    models comment

--- androidstories-app/src/main/java/com/igenius/androidstories/app/models/StoriesFolder.kt
+++ androidstories-app/src/main/java/com/igenius/androidstories/app/models/StoriesFolder.kt
@@ -2,20 +2,34 @@ package com.igenius.androidstories.app.models
 
 import com.igenius.androidstories.app.models.StoryNode.Companion.DIVIDER
 
+/**
+ * Class to define a gorup of stories
+ * @param completePath is the path including the folder name,
+ *          The defaul value is the root path
+ */
 class StoriesFolder(
     override val completePath: String = ROOT_FOLDER
 ) : StoryNode {
 
     private val _children: MutableList<StoryNode> = mutableListOf()
 
+    // List of all children nodes ordered by CompareChildren
     val children get() = _children.toList().sortedWith(CompareChildren())
 
+    /**
+     * The complete folder path, including the folder name
+     */
     override val cleanPath: String
         get() = if (isRoot) ROOT_FOLDER
         else super.cleanPath
 
+    // is true if the folder doesn't have any parent (root)
     private val isRoot get() = completePath == ROOT_FOLDER
 
+    /**
+     * Update or add a new child node based on the given node's name
+     * @param node to add or update
+     */
     fun updateOrAdd(node: StoryNode): Boolean {
         val toUpdate = _children.find { it.completePath == node.completePath }
         return toUpdate?.let {
@@ -29,6 +43,11 @@ class StoriesFolder(
         }
     }
 
+    /**
+     * Create or returns the fodler with the given name
+     * @param name of the subfolder
+     * @returns the subfolder that matches the given name or a new folder inserted as child
+     */
     fun getOrGenerateSubfolder(name: String): StoriesFolder =
         (if (!isRoot) "$completePath$DIVIDER$name" else name)
             .let { completePath ->
@@ -38,23 +57,39 @@ class StoriesFolder(
                     }
             }
 
+    /**
+     * Returns the number of levels to the given folder
+     * @param child to measure the distance
+     * @returns distance of the given folder,
+     *      -1 if not present inside the folders tree,
+     *      0 if the node is the same
+     */
     fun distance(child: StoryNode) =
         if (child == this) 0
         else if (!isParent(child)) -1
         else child.cleanPath.removePrefix("$cleanPath$DIVIDER").split(DIVIDER).size
 
+    /**
+     * @param child to check
+     * @returns true if the given child is present in the folder's tree
+     */
     fun isParent(child: StoryNode) = if (isRoot) true
     else child.cleanPath.startsWith("$cleanPath${DIVIDER}")
 
     private companion object {
+        // Path of root
         const val ROOT_FOLDER = ""
     }
 }
 
+/**
+ * Class used to order a list of nodes givin priority to the type (folders first)
+ * and in a second moment to the name
+ */
 private class CompareChildren : Comparator<StoryNode> {
     override fun compare(a: StoryNode, b: StoryNode): Int {
         val byType = compareType(a, b)
-        return if(byType == 0) compareName(a, b) else byType
+        return if (byType == 0) compareName(a, b) else byType
     }
 
     private fun compareType(a: StoryNode, b: StoryNode): Int = when {
--- androidstories-app/src/main/java/com/igenius/androidstories/app/models/StoriesProvider.kt
+++ androidstories-app/src/main/java/com/igenius/androidstories/app/models/StoriesProvider.kt
@@ -4,38 +4,75 @@ import android.content.Context
 import androidx.fragment.app.Fragment
 import com.igenius.androidstories.annotations.AsyncVariantProvider
 import com.igenius.androidstories.app.AsyncStoryFragment
-import java.lang.IllegalStateException
 
+/**
+ * Interface used by the processor to provide stories to the stories-app
+ */
 interface StoriesProvider {
     val stories: List<AndroidFragmentStory>
 }
 
+/**
+ * Generic story type
+ */
 interface AndroidStory : StoryNode {
+
+    // unique story identifier
     val id: Int
+
+    // story description
     val description: String?
+
+    /**
+     * List of variants, used to create the variants meno of a story,
+     * If the defined story is com.igenius.androidstories.app.StoryFragment,
+     * when the user will select one of these, onVariantSelected will be invoked
+     */
     val variants: Array<String>
 }
 
-interface AndroidFragmentStory: AndroidStory {
+/**
+ * Defines a story of a Fragment
+ */
+interface AndroidFragmentStory : AndroidStory {
     fun generateFragment(): Fragment
 }
 
-interface AndroidAsyncFragmentStory<T>: AndroidFragmentStory {
+/**
+ * Defines a story of a AndroidFragmentStory,
+ * @param <T> type of the async loaded data for a variant
+ */
+interface AndroidAsyncFragmentStory<T> : AndroidFragmentStory {
+
+    // returns an instance of a AsyncStoryFragment<T> (implemented by the processor)
     override fun generateFragment(): AsyncStoryFragment<T>
+
+    // instance of a variant provider
     val dataProvider: AsyncVariantProvider<T>
 }
 
+/**
+ * Class that defines a provider of data that receives the current android.content.Context
+ * @param <T> Type of the provided data
+ * @param cacheLifeTime is the life time specification of fetched data (@CacheLifeTime enum)
+ */
 abstract class AsyncContextVariantProvider<T>(
     cacheLifeTime: CacheLifeTime = CacheLifeTime.VIEW_MODEL
-): AsyncVariantProvider<T>(cacheLifeTime) {
+) : AsyncVariantProvider<T>(cacheLifeTime) {
+
+    /**
+     * Method to fetch data for a provided variant
+     * @param context
+     * @param variant to fetch
+     * @returns fetched data
+     */
+    abstract suspend fun provide(context: Context, variant: String): T
 
     suspend fun fetchData(context: Context, variant: String): T {
         cached(variant)?.let { return@fetchData it }
 
         return provide(context, variant).also { cache(variant, it) }
     }
-
-    abstract suspend fun provide(context: Context, variant: String): T
 
     final override suspend fun fetchData(variant: String): T {
         throw IllegalStateException("please use the fetchData method with context")
